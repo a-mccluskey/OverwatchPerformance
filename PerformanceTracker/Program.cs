924c2ab4362ac675d4c16d2fbf716c3bd6f88716
a-mccluskey <46331308+a-mccluskey@users.noreply.github.com>
2020-03-20 19:30:50 +0000

    .gitignore file

--- PerformanceTracker/Program.cs
+++ PerformanceTracker/Program.cs
@@ -10,6 +10,8 @@ namespace PerformanceTracker
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Testing");
+            Console.ReadKey();
         }
     }
 }
